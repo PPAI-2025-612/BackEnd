998135d91ed1699b5fdd147f7f12f04665ec8d10
sebacuaglia <sebacuaglia@gmail.com>
2025-06-11 21:53:45 -0300

    inicializacion repo

---
+++ redsismica/src/main/java/com/dsi/ppai/redsismica/RedsismicaApplication.java
@@ -0,0 +1,13 @@
+package com.dsi.ppai.redsismica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedsismicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedsismicaApplication.class, args);
+	}
+
+}
---
+++ redsismica/src/test/java/com/dsi/ppai/redsismica/RedsismicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsi.ppai.redsismica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedsismicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
