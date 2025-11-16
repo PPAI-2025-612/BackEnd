498e006e144a2f6bad4816d91f1c1f85240ce0c6
TadeoBaricco <tadeobaricco@gmail.com>
2025-11-15 22:48:07 -0300

    Solucion dependencias clase Sesion

--- redsismica/src/main/java/com/dsi/ppai/redsismica/model/Sesion.java
+++ redsismica/src/main/java/com/dsi/ppai/redsismica/model/Sesion.java
@@ -2,30 +2,32 @@ package com.dsi.ppai.redsismica.model;
 
 import java.util.Date;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lombok.Data;
+import lombok.NoArgsConstructor; // <--- 1. CRÍTICO: Requisito de JPA
 
 @Entity
-@Data 
+@Data
+@NoArgsConstructor // 1. CRÍTICO: Agregado para que Hibernate pueda crear instancias
 public class Sesion {
 
-	@Id
+    @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;  // <-- IDENTIFICADOR OBLIGATORIO
-	private Date fechaHoraDesde;
-	private Date fechaHoraHasta;
-	@ManyToOne
+    private Date fechaHoraDesde;
+    private Date fechaHoraHasta;
+    
+    @ManyToOne
     @JoinColumn(name = "usuario_id") // FK a tabla usuario
-	private Usuario usuario;
-	
-	
-	public Usuario getRILogueado() {
-		return usuario.getEmpleado();
-	}
+    private Usuario usuario;
+    
+    
+    public Usuario getRILogueado() {
+        return usuario.getEmpleado();
+    }
 }
